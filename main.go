9b6241be0640de8028cda7329e9708825ab91e2a
shirou <shirou.faw@gmail.com>
2018-04-08 15:59:18 +0900

    add search config feature

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 func main() {
-	config, err := NewConfig("config.json")
+	var confFile string
+	flag.StringVar(&confFile, "c", "", "config file path")
+	flag.Parse()
+	if confFile == "" {
+		path, err := os.Executable()
+		if err != nil {
+			log.Fatal(err)
+		}
+		path = filepath.Dir(path)
+		c, err := searchConfigFile(path)
+		if err != nil {
+			log.Fatal(fmt.Errorf("could not find config file on " + path))
+		}
+		confFile = c
+	}
+
+	config, err := NewConfig(confFile)
+
 	if err != nil {
 		log.Fatal(fmt.Errorf("config file error: %s", err))
 	}
@@ -25,6 +45,33 @@ func main() {
 		}
 		log.Printf("done")
 	}
+}
+
+func searchConfigFile(dir string) (string, error) {
+	glob := filepath.Join(dir, "*.json")
+
+	files, err := filepath.Glob(glob)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if files == nil {
+		return "", fmt.Errorf("no matching json file: " + dir)
+	}
+
+	for _, file := range files {
+		b, err := ioutil.ReadFile(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		s := string(b)
+		if strings.Contains(s, "generators") &&
+			strings.Contains(s, "output") &&
+			strings.Contains(s, "templates") {
+			return file, nil
+		}
+	}
+
+	return "", fmt.Errorf("no matching json file: " + dir)
 }
 
 func DirExists(dir string) error {
